4f4a6447be16789a36c5826679ecf2b460eaa353
kiriles90 <38388670+kiriles90@users.noreply.github.com>
2025-06-08 01:10:54 +0300

    Update violentmonkey.js

--- violentmonkey.js
+++ violentmonkey.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name        Bing to Google
 // @namespace   github.com/kiriles90
-// @version     1.1
-// @date        2023-02-06
+// @version     2.0
+// @date        2025-06-08
 // @author      github.com/kiriles90
 // @updateURL   https://raw.githubusercontent.com/kiriles90/Bing-to-google/master/violentmonkey.js
 // @downloadURL https://raw.githubusercontent.com/kiriles90/Bing-to-google/master/violentmonkey.js
@@ -11,5 +11,58 @@
 // @run-at      document-start
 // @grant       none
 // ==/UserScript==
-var newurl = "https://google.com/search?"+document.URL.match(/q\=[^&]*/);
-if (newurl != document.URL) location.replace(newurl);
+var query = document.URL.match(/q\=[^&]*/).toString().replace("q=", "");
+var newurl;
+function selectEngine(term) {
+    return query.includes(term);
+}
+switch (true) {
+    case selectEngine(term = encodeURIComponent("1337x:")):
+        newurl = "https://1337x.to/search/"+query.replace(term + "+", "")+"/1/";
+        break;
+    case selectEngine(term = encodeURIComponent("coverapi:")):
+        newurl = "https://tv643.ct.ws/coverapi/#"+query.replace(term + "+", "");
+        break;
+    case selectEngine(term = encodeURIComponent("eztv:")):
+        newurl = "https://eztvx.to/search/"+query.replace(term + "+", "");
+        break;
+    case selectEngine(term = encodeURIComponent("maps:")):
+        newurl = "https://www.google.com/maps/search/"+query.replace(term + "+", "")+"/";
+        break;
+    case selectEngine(term = encodeURIComponent("imdb:")):
+        newurl = "https://www.imdb.com/find?q="+query.replace(term + "+", "");
+        break;
+    case selectEngine(term = encodeURIComponent("nyaa:")):
+        newurl = "https://nyaa.si/?f=0&c=0_0&q="+query.replace(term + "+", "");
+        break;
+    case selectEngine(term = encodeURIComponent("opensubtitles:")):
+        newurl = "https://www.opensubtitles.org/en/search2/moviename-"+query.replace(term + "+", "")+"/sublanguageid-all";
+        break;
+    case selectEngine(term = encodeURIComponent("rlsbb:")):
+        newurl = "https://search.rlsbb.ru/?s="+query.replace(term + "+", "");
+        break;
+    case selectEngine(term = encodeURIComponent("scene-rls:")):
+        newurl = "https://scene-rls.net/?s="+query.replace(term + "+", "");
+        break;
+    case selectEngine(term = encodeURIComponent("solidtorrents:")):
+        newurl = "https://solidtorrents.to/search?q="+query.replace(term + "+", "")+"&sort=seeders";
+        break;
+    case selectEngine(term = encodeURIComponent("thepiratebay:")):
+        newurl = "https://thepiratebay.org/search.php?q="+query.replace(term + "+", "")+"&all=on&search=Pirate+Search&page=0";
+        break;
+    case selectEngine(term = encodeURIComponent("therarbg:")):
+        newurl = "https://therarbg.to/get-posts/order:-se:keywords:"+query.replace(term + "+", "")+"/";
+        break;
+    case selectEngine(term = encodeURIComponent("yourbittorrent:")):
+        newurl = "https://yourbittorrent.com/?v=&c=&q="+query.replace(term + "+", "");
+        break;
+    case selectEngine(term = encodeURIComponent("youtube:")):
+        newurl = "https://www.youtube.com/results?search_query="+query.replace(term + "+", "");
+        break;
+    case selectEngine(term = encodeURIComponent("yts:")):
+        newurl = "https://yts.mx/browse-movies/"+query.replace(term + "+", "")+"/all/all/0/latest/0/all";
+        break;
+    default:
+        newurl = "https://google.com/search?q="+query;
+}
+location.replace(newurl);
