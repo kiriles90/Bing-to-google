ff9c83370d7d239ef99f4ad4d2355fe24d9e5b78
kiriles90 <38388670+kiriles90@users.noreply.github.com>
2025-06-08 05:11:57 +0300

    Update violentmonkey.js

--- violentmonkey.js
+++ violentmonkey.js
@@ -1,66 +1,36 @@
 // ==UserScript==
 // @name        Bing to Google
 // @namespace   github.com/kiriles90
-// @version     2.1
-// @date        2025-06-09
+// @version     2.2
+// @date        2025-06-10
 // @author      github.com/kiriles90
 // @updateURL   https://raw.githubusercontent.com/kiriles90/Bing-to-google/master/violentmonkey.js
 // @downloadURL https://raw.githubusercontent.com/kiriles90/Bing-to-google/master/violentmonkey.js
-// @include     http://*.bing.com/search?*
-// @include     https://*.bing.com/search?*
+// @include     *://*.bing.com/search?*
 // @run-at      document-start
 // @grant       none
 // ==/UserScript==
-var query = document.URL.match(/q\=[^&]*/).toString().replace("q=", "");
-var engine = query.split("%3A+")[0];
-var newurl;
-switch (engine) {
-    case "1337x":
-        newurl = "https://1337x.to/search/"+query.replace(engine + "%3A+", "")+"/1/";
-        break;
-    case "coverapi":
-        newurl = "https://tv643.ct.ws/coverapi/#"+query.replace(engine + "%3A+", "");
-        break;
-    case "eztv":
-        newurl = "https://eztvx.to/search/"+query.replace(engine + "%3A+", "");
-        break;
-    case "maps":
-        newurl = "https://www.google.com/maps/search/"+query.replace(engine + "%3A+", "")+"/";
-        break;
-    case "imdb":
-        newurl = "https://www.imdb.com/find?q="+query.replace(engine + "%3A+", "");
-        break;
-    case "nyaa":
-        newurl = "https://nyaa.si/?f=0&c=0_0&q="+query.replace(engine + "%3A+", "");
-        break;
-    case "opensubtitles":
-        newurl = "https://www.opensubtitles.org/en/search2/moviename-"+query.replace(engine + "%3A+", "")+"/sublanguageid-all";
-        break;
-    case "rlsbb":
-        newurl = "https://search.rlsbb.ru/?s="+query.replace(engine + "%3A+", "");
-        break;
-    case "scene-rls":
-        newurl = "https://scene-rls.net/?s="+query.replace(engine + "%3A+", "");
-        break;
-    case "solidtorrents":
-        newurl = "https://solidtorrents.to/search?q="+query.replace(engine + "%3A+", "")+"&sort=seeders";
-        break;
-    case "thepiratebay":
-        newurl = "https://thepiratebay.org/search.php?q="+query.replace(engine + "%3A+", "")+"&all=on&search=Pirate+Search&page=0";
-        break;
-    case "therarbg":
-        newurl = "https://therarbg.to/get-posts/order:-se:keywords:"+query.replace(engine + "%3A+", "")+"/";
-        break;
-    case "yourbittorrent":
-        newurl = "https://yourbittorrent.com/?v=&c=&q="+query.replace(engine + "%3A+", "");
-        break;
-    case "youtube":
-        newurl = "https://www.youtube.com/results?search_query="+query.replace(engine + "%3A+", "");
-        break;
-    case "yts":
-        newurl = "https://yts.mx/browse-movies/"+query.replace(engine + "%3A+", "")+"/all/all/0/latest/0/all";
-        break;
-    default:
-        newurl = "https://google.com/search?q="+query;
-}
-location.replace(newurl);
+(() => {
+    const q = new URL(location).searchParams.get('q') || '',
+          [engine, ...rest] = q.split(': '),
+          term = encodeURIComponent(rest.join(': ') || ''),
+          map = {
+              '1337x': s => `https://1337x.to/search/${s}/1/`,
+              'coverapi': s => `https://tv643.ct.ws/coverapi/#${s}`,
+              'eztv': s => `https://eztvx.to/search/${s}`,
+              'maps': s => `https://www.google.com/maps/search/${s}/`,
+              'imdb': s => `https://www.imdb.com/find?q=${s}`,
+              'nyaa': s => `https://nyaa.si/?f=0&c=0_0&q=${s}`,
+              'opensubtitles': s => `https://www.opensubtitles.org/en/search2/moviename-${s}/sublanguageid-all`,
+              'rlsbb': s => `https://search.rlsbb.ru/?s=${s}`,
+              'scene-rls': s => `https://scene-rls.net/?s=${s}`,
+              'solidtorrents': s => `https://solidtorrents.to/search?q=${s}&sort=seeders`,
+              'thepiratebay': s => `https://thepiratebay.org/search.php?q=${s}&all=on`,
+              'therarbg': s => `https://therarbg.to/get-posts/order:-se:keywords:${s}/`,
+              'yourbittorrent': s => `https://yourbittorrent.com/?q=${s}`,
+              'youtube': s => `https://www.youtube.com/results?search_query=${s}`,
+              'yts': s => `https://yts.mx/browse-movies/${s}/all/all/0/latest/0/all`
+          },
+          url = (map[engine] || (() => `https://google.com/search?q=${encodeURIComponent(q)}`))(term);
+    location.replace(url);
+})();
