d75589b174f7d2c570fa9356acca83f4767b8656
kiriles90 <38388670+kiriles90@users.noreply.github.com>
2026-01-09 01:38:27 +0200

    Update violentmonkey.js

--- violentmonkey.js
+++ violentmonkey.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name        Bing to Google
 // @namespace   github.com/kiriles90
-// @version     4.9
-// @date        2026-01-08
+// @version     5.0
+// @date        2026-01-09
 // @author      github.com/kiriles90
 // @updateURL   https://raw.githubusercontent.com/kiriles90/Bing-to-google/master/violentmonkey.js
 // @downloadURL https://raw.githubusercontent.com/kiriles90/Bing-to-google/master/violentmonkey.js
